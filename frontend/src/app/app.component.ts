034b7b1e4e19cd7c5e44fd386013a44c1f97f5b4
Dominika <olszewska.dominika.private@gmail.com>
2021-03-03 09:53:33 +0100

    First enpoints created

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+
+export interface PostResponse {
+  message: string;
+  posts?: Post[];
+}
+
+export interface Post {
+  id: string;
+  title: string;
+  content: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'mean-course';
+export class AppComponent implements OnInit {
+  public title: string = 'mean-course';
+  public posts: Post[] = [];
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  public ngOnInit(): void {
+    this.http.get<PostResponse>('http://localhost:8000/api/posts').subscribe(response => {
+      this.posts = response.posts;
+    }
+
+    );
+  }
+
+  public postMessage(): void {
+    const post: Post= {
+      id: '1234',
+      title: 'Third server-side post',
+      content: 'This is coming from the angular app',
+    };
+    this.http.post<PostResponse>('http://localhost:8000/api/posts', post).subscribe(resonse => {
+      console.log('response', resonse);
+    });
+  }
+
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    CommonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
